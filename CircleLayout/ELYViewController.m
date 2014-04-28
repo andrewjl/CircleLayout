252f9faaf381a414b9fa475b53919b23685aa5bd
Andrew Barinov <andy.lauerbarinov@me.com>
2014-04-27 18:35:05 -0700

    Initial Commit

---
+++ CircleLayout/ELYViewController.m
@@ -0,0 +1,29 @@
+//
+//  ELYViewController.m
+//  CircleLayout
+//
+//  Created by Andrew Lauer Barinov on 4/27/14.
+//  Copyright (c) 2014 Elysian Fields. All rights reserved.
+//
+
+#import "ELYViewController.h"
+
+@interface ELYViewController ()
+
+@end
+
+@implementation ELYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
